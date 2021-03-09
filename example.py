cf4f1aaeb37f2416635afd89eb9acc4fb51606ff
yiwengao <yiwen.gao@columbia.edu>
2021-03-09 16:14:05 -0600

    update examples

--- example.py
+++ example.py
@@ -4,24 +4,30 @@ import time
 
 
 @pydron.functional
-def foo(num):
+def foo1(num):
     time.sleep(5)
     return num
     # return num + math.log(num)
+
+
+@pydron.functional
+def foo2(num):
+    time.sleep(5)
+    return num
 
 
 @pydron.schedule
 def calibration_pipeline(inputs):
     outputs = []
     for num in inputs:
-        output = foo(num)
+        output = foo1(num) + foo2(num)
         outputs = outputs + [output]
     return outputs
 
 
 if __name__ == '__main__':
     start = time.time()
-    vals = [n for n in range(10)]
+    vals = [n for n in range(5)]
     print(calibration_pipeline(vals))
     end = time.time()
     print(end - start)
